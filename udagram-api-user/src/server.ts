dbc464e462ed1f78c370f1a9e98a10107ff330b0
Mahaba98 <sokhnadiarrasene68@gmail.com>
2022-10-06 14:11:21 +0000

    déploiement

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -2,11 +2,11 @@ import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
 
-import {IndexRouter} from './controllers/v0/index.router';
+import {IndexRouter} from './controllers/v0/users/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_USER_MODELS} from './controllers/model.index';
 
 
 (async () => {
