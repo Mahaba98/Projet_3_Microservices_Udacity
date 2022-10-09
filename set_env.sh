caac7983a79c07cac616b620c5a3a53c5002baeb
Mahaba98 <sokhnadiarrasene68@gmail.com>
2022-10-08 13:17:09 -1200

    Travis

--- set_env.sh
+++ set_env.sh
@@ -2,8 +2,8 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=mariama
 export POSTGRES_PASSWORD=Balygueye7
-export POSTGRES_HOST= postgres.chbrcxrn6poy.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=database_user_info
+export POSTGRES_HOST=database-user-info.chbrcxrn6poy.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres 
 export AWS_BUCKET=images-524757988542-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
